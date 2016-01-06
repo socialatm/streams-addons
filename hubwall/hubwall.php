83f75375945b91c01ee9e582af66ffffad357503
redmatrix <git@macgirvin.com>
2016-01-05 17:11:44 -0800

    improvements to hubwall

--- hubwall/hubwall.php
+++ hubwall/hubwall.php
@@ -33,8 +33,10 @@ function hubwall_post(&$a) {
 
 	$htmlversion = bbcode(stripslashes(str_replace(array("\\r","\\n"), array("","<br />\n"),$text)));
 
+	$sql_extra = ((intval($_REQUEST['test'])) ? sprintf(" and account_email = '%s' ", get_config('system','admin_email')) : ''); 
 
-	$recips = q("select account_email from account where account_flags = %d",
+
+	$recips = q("select account_email from account where account_flags = %d $sql_extra",
 		intval(ACCOUNT_OK)
 	);
 
@@ -67,7 +69,9 @@ function hubwall_content(&$a) {
 
 	$o = replace_macros(get_markup_template('hubwall_form.tpl','addon/hubwall/'),array(
 		'$title' => $title,
-		'$subject' => array('subject',t('Message subject'),'',''),
+		'$text' => htmlspecialchars($_REQUEST['text']),
+		'$subject' => array('subject',t('Message subject'),$_REQUEST['subject'],''),
+		'$test' => array('test',t('Test mode (only send to hub administrator)'), 0,''),
 		'$submit' => t('Submit')
 	));
 
--- hubwall/view/tpl/hubwall_form.tpl
+++ hubwall/view/tpl/hubwall_form.tpl
@@ -2,9 +2,10 @@
 
 <form action="hubwall" method="post">
 
+{{include file="field_checkbox.tpl" field=$test}}
 {{include file="field_input.tpl" field=$subject}}
 
-<textarea name="text"></textarea>
+<textarea name="text" style="width:100%; height:150px;">{{$text}}</textarea>
 <br />
 <input type="submit" name="submit" value="{{$submit}}" />
 </form>
