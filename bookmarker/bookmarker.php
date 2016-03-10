b0534ced6f062a54a6c36fab1b4dba28feee08ac
jeroenpraat <jeroenpraat@xs4all.nl>
2016-03-10 17:18:38 +0100

    This works too

--- bookmarker/bookmarker.php
+++ bookmarker/bookmarker.php
@@ -28,9 +28,7 @@ function bookmarker_prepare_body(&$a,&$b) {
 	if(! strpos($b['html'],'bookmark-identifier'))
 		return;
 
-	$base_theme = get_app()->theme_info['extends'];
-	
-	if(function_exists('redbasic_init') || $base_theme == 'redbasic')
+	if(function_exists('redbasic_init') || get_app()->theme_info['extends'] == 'redbasic')
 		$bookmarkicon = '<i class="icon-bookmark"></i>';
 	else 
 		$bookmarkicon = '<img src="addon/bookmarker/bookmarker.png" width="19px" height="20px" alt="#^" />';
