4d7e784e6e2217c8ece2d26704779b9f8265108b
redmatrix <git@macgirvin.com>
2016-05-15 17:59:52 -0700

    $item -> $target_item, update nodeinfo with new spec for 'zot' as protocol

--- diaspora/diaspora.php
+++ diaspora/diaspora.php
@@ -123,7 +123,7 @@ function diaspora_notifier_process(&$a,&$arr) {
 
 	// if it is a public post (reply, etc.), add the chosen relay channel to the recipients
 
-	if(! array_key_exists('item_wall',$arr['item']))
+	if(! array_key_exists('item_wall',$arr['target_item']))
 		return;
 
 	if(($arr['normal_mode']) && (! $arr['env_recips']) && (! $arr['private']) && (! $arr['relay_to_owner'])) {
