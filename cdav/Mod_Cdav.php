2f2fc452dd1b02c2f26355ab5d70002c1b93efe3
Mario Vavti <mario@mariovavti.com>
2016-06-06 09:53:24 +0200

    move cdav to module class

---
+++ cdav/Mod_Cdav.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Zotlabs\Module;
+
+class Cdav extends \Zotlabs\Web\Controller {
+
+	function init() {
+	
+			if(\DBA::$dba && \DBA::$dba->connected)
+				$pdovars = \DBA::$dba->pdo_get();
+			else
+				killme();
+
+			// workaround for HTTP-auth in CGI mode
+			if (x($_SERVER, 'REDIRECT_REMOTE_USER')) {
+				$userpass = base64_decode(substr($_SERVER["REDIRECT_REMOTE_USER"], 6)) ;
+				if(strlen($userpass)) {
+					list($name, $password) = explode(':', $userpass);
+					$_SERVER['PHP_AUTH_USER'] = $name;
+					$_SERVER['PHP_AUTH_PW'] = $password;
+				}
+			}
+
+			if (x($_SERVER, 'HTTP_AUTHORIZATION')) {
+				$userpass = base64_decode(substr($_SERVER["HTTP_AUTHORIZATION"], 6)) ;
+				if(strlen($userpass)) {
+					list($name, $password) = explode(':', $userpass);
+					$_SERVER['PHP_AUTH_USER'] = $name;
+					$_SERVER['PHP_AUTH_PW'] = $password;
+				}
+			}
+
+			/**
+			 * This server combines both CardDAV and CalDAV functionality into a single
+			 * server. It is assumed that the server runs at the root of a HTTP domain (be
+			 * that a domainname-based vhost or a specific TCP port.
+			 *
+			 * This example also assumes that you're using SQLite and the database has
+			 * already been setup (along with the database tables).
+			 *
+			 * You may choose to use MySQL instead, just change the PDO connection
+			 * statement.
+			 */
+
+			/**
+			 * UTC or GMT is easy to work with, and usually recommended for any
+			 * application.
+			 */
+			date_default_timezone_set('UTC');
+
+			/**
+			 * Make sure this setting is turned on and reflect the root url for your WebDAV
+			 * server.
+			 *
+			 * This can be for example the root / or a complete path to your server script.
+			 */
+
+			$baseUri = '/cdav/';
+
+			/**
+			 * Database
+			 *
+			 */
+
+			$pdo = new \PDO($pdovars[0],$pdovars[1],$pdovars[2]);
+			$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+
+			// Autoloader
+			require_once 'vendor/autoload.php';
+
+			/**
+			 * The backends. Yes we do really need all of them.
+			 *
+			 * This allows any developer to subclass just any of them and hook into their
+			 * own backend systems.
+			 */
+
+			$auth = new \Zotlabs\Storage\BasicAuth();
+			$auth->setRealm(ucfirst(\Zotlabs\Lib\System::get_platform_name()) . 'CalDAV/CardDAV');
+
+			//$authBackend      = new \Sabre\DAV\Auth\Backend\PDO($pdo);
+			$principalBackend = new \Sabre\DAVACL\PrincipalBackend\PDO($pdo);
+			$carddavBackend   = new \Sabre\CardDAV\Backend\PDO($pdo);
+			$caldavBackend    = new \Sabre\CalDAV\Backend\PDO($pdo);
+
+			/**
+			 * The directory tree
+			 *
+			 * Basically this is an array which contains the 'top-level' directories in the
+			 * WebDAV server.
+			 */
+
+			$nodes = [
+				// /principals
+				new \Sabre\CalDAV\Principal\Collection($principalBackend),
+				// /calendars
+				new \Sabre\CalDAV\CalendarRoot($principalBackend, $caldavBackend),
+				// /addressbook
+				new \Sabre\CardDAV\AddressBookRoot($principalBackend, $carddavBackend),
+			];
+
+			// The object tree needs in turn to be passed to the server class
+
+			$server = new \Sabre\DAV\Server($nodes);
+
+			if(isset($baseUri))
+				$server->setBaseUri($baseUri);
+
+			// Plugins
+			$server->addPlugin(new \Sabre\DAV\Auth\Plugin($auth));
+			$server->addPlugin(new \Sabre\DAV\Browser\Plugin());
+			$server->addPlugin(new \Sabre\DAV\Sync\Plugin());
+			$server->addPlugin(new \Sabre\DAV\Sharing\Plugin());
+			$server->addPlugin(new \Sabre\DAVACL\Plugin());
+
+			// CalDAV plugins
+			$server->addPlugin(new \Sabre\CalDAV\Plugin());
+			//$server->addPlugin(new \Sabre\CalDAV\Schedule\Plugin());
+			$server->addPlugin(new \Sabre\CalDAV\ICSExportPlugin());
+
+			// CardDAV plugins
+			$server->addPlugin(new \Sabre\CardDAV\Plugin());
+			$server->addPlugin(new \Sabre\CardDAV\VCFExportPlugin());
+
+			// And off we go!
+			$server->exec();
+
+			killme();
+
+		}
+
+	}
+
+}
--- cdav/cdav.php
+++ cdav/cdav.php
@@ -8,6 +8,8 @@
  * 
  */
 
+require_once('addon/cdav/Mod_Cdav.php');
+
 function cdav_install() {
 
 	if(ACTIVE_DBTYPE === DBTYPE_POSTGRES) {
@@ -35,7 +37,6 @@ function cdav_install() {
 	}
 }
 
-
 function cdav_uninstall() {
 	// Currently we do nothing here as it could destroy a lot of data, when
 	// often you only want to reset the plugin state. 
@@ -50,20 +51,15 @@ function cdav_load() {
 
 }
 
-
 function cdav_unload() {
 	Zotlabs\Extend\Hook::unregister_by_file('addon/cdav/cdav.php');
 }
-
 
 function cdav_well_known(&$x) {
 	if(argv(1) === 'caldav' || argv(1) === 'carddav') {
 		goaway(z_root() . '/cdav');
 	}
 }
-
-
-
 
 function cdav_feature_settings_post(&$b) {
 
@@ -118,7 +114,6 @@ function cdav_feature_settings_post(&$b) {
 	}
 }
 
-
 function cdav_feature_settings(&$b) {
 
 	$channel = App::get_channel();
@@ -146,151 +141,4 @@ function cdav_feature_settings(&$b) {
 		'$addon'    => array('cdav', t('CalDAV/CardDAV Settings'), '', t('Submit')),
 		'$content'  => $sc
 	));
-}
-
-function cdav_module() {}
-
-function cdav_init(&$a) {
-
-	if(DBA::$dba && DBA::$dba->connected)
-		$pdovars = DBA::$dba->pdo_get();
-	else
-		killme();
-
-
-	// workaround for HTTP-auth in CGI mode
-	if (x($_SERVER, 'REDIRECT_REMOTE_USER')) {
-		$userpass = base64_decode(substr($_SERVER["REDIRECT_REMOTE_USER"], 6)) ;
-		if(strlen($userpass)) {
-			list($name, $password) = explode(':', $userpass);
-			$_SERVER['PHP_AUTH_USER'] = $name;
-			$_SERVER['PHP_AUTH_PW'] = $password;
-		}
-	}
-
-	if (x($_SERVER, 'HTTP_AUTHORIZATION')) {
-		$userpass = base64_decode(substr($_SERVER["HTTP_AUTHORIZATION"], 6)) ;
-		if(strlen($userpass)) {
-			list($name, $password) = explode(':', $userpass);
-			$_SERVER['PHP_AUTH_USER'] = $name;
-			$_SERVER['PHP_AUTH_PW'] = $password;
-		}
-	}
-
-	/**
-	 * This server combines both CardDAV and CalDAV functionality into a single
-	 * server. It is assumed that the server runs at the root of a HTTP domain (be
-	 * that a domainname-based vhost or a specific TCP port.
-	 *
-	 * This example also assumes that you're using SQLite and the database has
-	 * already been setup (along with the database tables).
-	 *
-	 * You may choose to use MySQL instead, just change the PDO connection
-	 * statement.
-	 */
-
-	/**
-	 * UTC or GMT is easy to work with, and usually recommended for any
-	 * application.
-	 */
-	date_default_timezone_set('UTC');
-
-	/**
-	 * Make sure this setting is turned on and reflect the root url for your WebDAV
-	 * server.
-	 *
-	 * This can be for example the root / or a complete path to your server script.
-	 */
-
-	$baseUri = '/cdav';
-
-	/**
-	 * Database
-	 *
-	 */
-
-	$pdo = new \PDO($pdovars[0],$pdovars[1],$pdovars[2]);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	/**
-	 * Mapping PHP errors to exceptions.
-	 *
-	 * While this is not strictly needed, it makes a lot of sense to do so. If an
-	 * E_NOTICE or anything appears in your code, this allows SabreDAV to intercept
-	 * the issue and send a proper response back to the client (HTTP/1.1 500).
-	 */
-
-	function exception_error_handler($errno, $errstr, $errfile, $errline) {
-		throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
-	}
-
-	set_error_handler("exception_error_handler");
-
-	// Autoloader
-	require_once 'vendor/autoload.php';
-
-	/**
-	 * The backends. Yes we do really need all of them.
-	 *
-	 * This allows any developer to subclass just any of them and hook into their
-	 * own backend systems.
-	 */
-
-	$auth = new \Zotlabs\Storage\BasicAuth();
-	$auth->setRealm(ucfirst(\Zotlabs\Lib\System::get_platform_name()) . 'CalDAV/CardDAV');
-
-	//$authBackend      = new \Sabre\DAV\Auth\Backend\PDO($pdo);
-	$principalBackend = new \Sabre\DAVACL\PrincipalBackend\PDO($pdo);
-	$carddavBackend   = new \Sabre\CardDAV\Backend\PDO($pdo);
-	$caldavBackend    = new \Sabre\CalDAV\Backend\PDO($pdo);
-
-	/**
-	 * The directory tree
-	 *
-	 * Basically this is an array which contains the 'top-level' directories in the
-	 * WebDAV server.
-	 */
-
-	$nodes = [
-		// /principals
-		new \Sabre\CalDAV\Principal\Collection($principalBackend),
-		// /calendars
-		new \Sabre\CalDAV\CalendarRoot($principalBackend, $caldavBackend),
-		// /addressbook
-		new \Sabre\CardDAV\AddressBookRoot($principalBackend, $carddavBackend),
-	];
-
-	// The object tree needs in turn to be passed to the server class
-
-	$server = new \Sabre\DAV\Server($nodes);
-
-	if(isset($baseUri))
-		$server->setBaseUri($baseUri);
-
-	// Plugins
-	$server->addPlugin(new \Sabre\DAV\Auth\Plugin($auth));
-
-//	$browser = new \Zotlabs\Storage\Browser($auth);
-//	$auth->setBrowserPlugin($browser);
-	
-//	$server->addPlugin($browser);
-
-	$server->addPlugin(new \Sabre\DAV\Browser\Plugin());
-	$server->addPlugin(new \Sabre\DAV\Sync\Plugin());
-	$server->addPlugin(new \Sabre\DAV\Sharing\Plugin());
-	$server->addPlugin(new \Sabre\DAVACL\Plugin());
-
-	// CalDAV plugins
-	$server->addPlugin(new \Sabre\CalDAV\Plugin());
-	//$server->addPlugin(new \Sabre\CalDAV\Schedule\Plugin());
-	$server->addPlugin(new \Sabre\CalDAV\ICSExportPlugin());
-
-	// CardDAV plugins
-	$server->addPlugin(new \Sabre\CardDAV\Plugin());
-	$server->addPlugin(new \Sabre\CardDAV\VCFExportPlugin());
-
-	// And off we go!
-	$server->exec();
-
-	killme();
 }
