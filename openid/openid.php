691f87620fe0f47560c1ebcdfc5fb17575e7885b
redmatrix <git@macgirvin.com>
2016-07-27 22:30:12 -0700

    finish removing openid from core

--- openid/openid.php
+++ openid/openid.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('addon/openid/Mod_Openid.php');
+require_once('library/openid/openid.php');
 
 
 	/**
@@ -10,11 +10,12 @@ require_once('addon/openid/Mod_Openid.php');
 	 * Version: 1.0
 	 * Author: Mike Macgirvin
 	 *
-     */
+	 */
 
 
 function openid_load() {
 	Zotlabs\Extend\Hook::register('module_loaded','addon/openid/openid.php','openid_module_loaded');
+	Zotlabs\Extend\Hook::register('reverse_magic_auth','addon/openid/openid.php','openid_reverse_magic_auth');
 }
 
 function openid_unload() {
@@ -23,9 +24,30 @@ function openid_unload() {
 
 
 function openid_module_loaded(&$x) {
+	if($x['module'] === 'openid') {
+		require_once('addon/openid/Mod_Openid.php');
+		$x['controller'] = new \Zotlabs\Module\Openid();
+		$x['installed'] = true;
+	}
 	if($x['module'] === 'id') {
 		require_once('addon/openid/Mod_Id.php');
 		$x['controller'] = new \Zotlabs\Module\Id();
 		$x['installed'] = true;
 	}
+}
+
+function openid_reverse_magic_auth($x) {
+
+	try {
+		$openid = new \LightOpenID(z_root());
+		$openid->identity = $x['address'];
+		$openid->returnUrl = z_root() . '/openid';
+		$openid->required =  [ 'namePerson/friendly', 'namePerson' ];
+		$openid->optional = [ 'namePerson/first', 'media/image/aspect11', 'media/image/default' ];
+		goaway($openid->authUrl());
+	}
+	catch (\Exception $e) {
+		notice( t('We encountered a problem while logging in with the OpenID you provided. Please check the correct spelling of the ID.').'<br /><br >'. t('The error message was:').' '.$e->getMessage());
+	}
+
 }
