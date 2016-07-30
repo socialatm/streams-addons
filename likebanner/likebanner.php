10d01327cd79b02bde2609f605b7ec4c94d7eaad
redmatrix <git@macgirvin.com>
2016-07-30 15:49:54 -0700

    updated likebanner

--- likebanner/likebanner.php
+++ likebanner/likebanner.php
@@ -2,8 +2,8 @@
 
 /**
  * Name: Like Banner
- * Description: Creates a "like us on redmatrix" banner (obsolete)
- * Version: 1.0
+ * Description: Creates a "like us on hubzilla" banner 
+ * Version: 1.1
  * Author: Mike Macgirvin
  * Maintainer: none
  */
@@ -19,8 +19,8 @@ function likebanner_init(&$a) {
 		header("Content-Type: image/png");
 		$im = ImageCreateFromPng('addon/likebanner/like_banner.png');
 		$black = ImageColorAllocate($im, 0,0,0);
-		$start_x = 18;
-		$start_y = 110;
+		$start_x = 24;
+		$start_y = 70;
 		$fontsize=(($_REQUEST['size'])? intval($_REQUEST['size']) : 28);
 		imagettftext($im,$fontsize,0,$start_x,$start_y,$black, 'addon/likebanner/FreeSansBold.ttf',$_REQUEST['addr']);
 		imagepng($im);
@@ -53,7 +53,7 @@ function likebanner_content(&$a) {
 	$o .= '<input type="text" name="addr" size="32" value="' . $def . '" />';
 	$o .= '<br /><br />' . t('Fontsize (px):');
 	$o .= '<br /><br />';
-	$o .= '<input type="text" name="size" size="32" value="' . (($_REQUEST['size']) ? $_REQUEST['size'] : 28) . '" /><br /><br />';
+	$o .= '<input type="text" name="size" size="32" value="' . (($_REQUEST['size']) ? $_REQUEST['size'] : 12) . '" /><br /><br />';
 	$o .= '<input type="submit" name="submit" value="' . t('Submit'). '" /></form><br /><br/>';
 
 	if($_REQUEST['addr']) {
