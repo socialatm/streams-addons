4cff6cc83ffb64748fa98b4c9179c71dbe2fc642
Andrew Manning <tamanning@zoho.com>
2016-09-28 21:40:42 -0400

    Integrated leaflet-gps.js to add location tracking control.

--- rendezvous/view/js/rendezvous.js
+++ rendezvous/view/js/rendezvous.js
@@ -1,223 +1,34 @@
 // Declare the rendezvous namespace
 var rv = rv || {};
 
-/**
- * Elements that make up the popup.
- */
-rv.container = document.getElementById('popup');
-rv.content = document.getElementById('popup-content');
-rv.closer = document.getElementById('popup-closer');
 
-$('#add-marker').click(function(evt) { rv.createMarkerDialog(evt); });
-$('#recenter-control').click(function(evt) { rv.zoomToMyLocation(); });
-/**
- * Create an overlay to anchor the popup to the map.
- */
-rv.overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
-	element: rv.container,
-	autoPan: true,
-	autoPanAnimation: {
-		duration: 250
-	}
-}));
-/**
- * Add a click handler to hide the popup.
- * @return {boolean} Don't follow the href.
- */
-rv.closer.onclick = function () {
-	rv.overlay.setPosition(undefined);
-	rv.closer.blur();
-	return false;
-};
+rv.mymap = L.map('map').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'mapbox.streets'
+}).addTo(rv.mymap);
 
-/*
- // Marker feature
- rv.sharedMarker = new ol.Feature({
- geometry: new ol.geom.Point(ol.proj.fromLonLat([0,0]))
- });
- rv.sharedMarker.setStyle(new ol.style.Style({
- image: new ol.style.Circle({
- radius: 6,
- fill: new ol.style.Fill({
- color: '#FF0000'
- }),
- stroke: new ol.style.Stroke({
- color: '#fff',
- width: 2
- })
- })
- }));
- rv.vectorSource = new ol.source.Vector({
- features: [rv.sharedMarker]
- });
- 
- rv.vectorLayer = new ol.layer.Vector({
- source: rv.vectorSource
- });
- */
-rv.view = new ol.View({
-	center: ol.proj.transform([0, 0], 'EPSG:4326', 'EPSG:3857'),
-	zoom: 5
-});
+L.marker([51.5, -0.09]).addTo(rv.mymap)
+		.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
 
-// Create the map
-rv.map = new ol.Map({
-	overlays: [rv.overlay],
-	layers: [
-		new ol.layer.Tile({
-			source: new ol.source.TileWMS({
-				url: 'https://wms.jpl.nasa.gov/wms.cgi',
-				layers: 'modis'
-				
-			})
-		})
-	],
-	target: 'map',
-	view: rv.view
-});
-rv.map.addControl(new ol.control.ZoomSlider());
+rv.popup = L.popup();
 
-// Geolocation tracking
+function onMapClick(e) {
+	rv.popup
+			.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(rv.mymap);
+}
 
-rv.geolocation = new ol.Geolocation(/** @type {olx.GeolocationOptions} */ ({
-	projection: rv.view.getProjection(),
-	trackingOptions: {
-		maximumAge: 10000,
-		enableHighAccuracy: true,
-		timeout: 600000
-	}
-}));
+rv.mymap.on('click', onMapClick);
 
-// Listen to position changes
-rv.geolocation.on('change', function (evt) {
-	//rv.view.setCenter(rv.geolocation.getPosition());
-	//rv.view.setZoom(16);
-});
-
-rv.geolocation.on('error', function () {
-	alert('geolocation error');
-	// FIXME we should remove the coordinates in positions
-});
-
-
-
-rv.accuracyFeature = new ol.Feature();
-rv.accuracyFeature.bindTo('geometry', rv.geolocation, 'accuracyGeometry');
-
-rv.positionFeature = new ol.Feature({
-	type: 'mylocation',
-	name: 'Me'
-});
-
-rv.positionFeature.setStyle(new ol.style.Style({
-	image: new ol.style.Circle({
-		radius: 6,
-		fill: new ol.style.Fill({
-			color: '#3399CC'
-		}),
-		stroke: new ol.style.Stroke({
-			color: '#fff',
-			width: 2
-		})
-	})
-}));
-
-rv.positionFeature.bindTo('geometry', rv.geolocation, 'position')
-		.transform(function () {
-		}, function (coordinates) {
-			return coordinates ? new ol.geom.Point(coordinates) : null;
-		});
-// Create the user location layer
-rv.userLocationFeatures = new ol.source.Vector({
-	features: [rv.positionFeature, rv.accuracyFeature]
-});
-rv.userLocationLayer = new ol.layer.Vector({
-	source: rv.userLocationFeatures
-});
-rv.map.addLayer(rv.userLocationLayer);
-
-
-rv.markerLayerSource = new ol.source.Vector();
-rv.markerLayer = new ol.layer.Vector({
-	source: rv.markerLayerSource,
-	name: 'markers'
-});
-rv.map.addLayer(rv.markerLayer);
-
-// Map click handler
-rv.map.on('singleclick', function (evt) {
-	var coordinate = evt.coordinate;
-	//check for features
-	var feature = false;
-	rv.map.forEachFeatureAtPixel(rv.map.getPixelFromCoordinate(coordinate), function (f, layer) {
-		if (f.get('type') === 'mylocation') {
-			var lat = parseFloat(ol.proj.transform(f.getGeometry().getExtent(), 'EPSG:3857', 'EPSG:4326')[1]).toFixed(3);
-			var lon = parseFloat(ol.proj.transform(f.getGeometry().getExtent(), 'EPSG:3857', 'EPSG:4326')[0]).toFixed(3);
-			rv.content.innerHTML = f.get('name') + '<br>lat: ' + lat + '<br>lon: ' + lon;
-			rv.overlay.setPosition(coordinate);
-			feature = true;
-		} 
-		if (f.get('type') === 'marker') {
-			var lat = parseFloat(ol.proj.transform(f.getGeometry().getExtent(), 'EPSG:3857', 'EPSG:4326')[1]).toFixed(3);
-			var lon = parseFloat(ol.proj.transform(f.getGeometry().getExtent(), 'EPSG:3857', 'EPSG:4326')[0]).toFixed(3);
-			rv.content.innerHTML = f.get('name') + '<br>'+ f.get('description') +'<br>lat: ' + lat + '<br>lon: ' + lon;
-			rv.overlay.setPosition(coordinate);
-			feature = true;
-		} 
-	});
-	if (!feature) {
-		var lat = parseFloat(ol.proj.transform(coordinate, 'EPSG:3857', 'EPSG:4326')[1]).toFixed(3);
-		var lon = parseFloat(ol.proj.transform(coordinate, 'EPSG:3857', 'EPSG:4326')[0]).toFixed(3);
-		rv.overlay.setPosition(coordinate);
-	}
-});
-
-
-
-
-rv.createMarker = function (coords, name, description) {
-	var marker = new ol.Feature({
-		name: name || '',
-		description: description || '',
-		type: 'marker'
-	});
-	marker.setGeometry(new ol.geom.Point(coords));
-	marker.setStyle(new ol.style.Style({
-		image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-			anchor: [0.5, 30],
-			anchorXUnits: 'fraction',
-			anchorYUnits: 'pixels',
-			opacity: 0.75,
-			src: '/addon/rendezvous/view/img/marker_red_30px.png'
-		}))
-	}));
-	rv.markerLayerSource.addFeature(marker);
-};
-
-rv.createMarkerDialog = function (evt) {
-	$("#add-marker-dialog").dialog({
-		modal: true,
-		buttons: {
-			Ok: function () {
-				$(this).dialog("close");
-			},
-			Cancel: function () {
-				$(this).dialog("close");
-			}
-		}
-	});
-	
-};
-
-rv.zoomToMyLocation = function () {
-	rv.view.setCenter(rv.geolocation.getPosition());
-	rv.view.setZoom(16);
-};
-
+rv.mymap.addControl( new L.Control.Gps({marker: new L.Marker([0,0])}) );//inizialize control
 
 $(window).load(function () {
 
-	rv.geolocation.setTracking(true); // Start position tracking
 
 });
--- rendezvous/view/tpl/rendezvous.tpl
+++ rendezvous/view/tpl/rendezvous.tpl
@@ -2,34 +2,20 @@
 <html>
 		<head>
 				<title>{{$pagetitle}}</title>
-				<link href="/addon/rendezvous/view/css/ol.css" rel='stylesheet' type='text/css'>
-				<link rel="stylesheet" href="/addon/rendezvous/view/css/jquery-ui.css">
+				<link href="/addon/rendezvous/view/css/jquery-ui.css" rel='stylesheet' type='text/css'>
 				<link href="/addon/rendezvous/view/css/rendezvous.css" rel='stylesheet' type='text/css'>
-				
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="/addon/rendezvous/view/css/leaflet.css" rel='stylesheet' type='text/css'>
+				<link href="/addon/rendezvous/view/css/leaflet-gps.css" rel='stylesheet' type='text/css'>
 				<script src="/addon/rendezvous/view/js/jquery-1.12.4.js"></script>
 				<script src="/addon/rendezvous/view/js/jquery-ui.js"></script>
-				<script src="/addon/rendezvous/view/js/ol.js?v=0.1" ></script>
+
 		</head>
 		<body>
-				<div id="map" class="map">
-						<div id="recenter-control" class="ol-control"><img src='addon/map/view/img/center-control.png' width="20px"></div>
-				</div>
-				
-				<div id="popup" class="ol-popup">
-						<a href="#" id="popup-closer" class="ol-popup-closer"></a>
-						<div id="marker-popup-content">
-								<div id="add-marker" class="btn btn-default"><a href="">Create marker</a></div>
-						</div>
-						<div id="popup-content"></div>
-				</div>
-				 
-				<div id="add-marker-dialog" title="Add marker">
-					<p>
-						<span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
-						Create a new marker!
-					</p>
-				</div>
-				
-				<script src="/addon/rendezvous/view/js/rendezvous.js?v=0.1" ></script>
+				<div id="map" class="map"></div>
+
+				<script src="/addon/rendezvous/view/js/leaflet.js"></script>
+				<script src="/addon/rendezvous/view/js/leaflet-gps.js"></script>
+				<script src="/addon/rendezvous/view/js/rendezvous.js"></script>
 		</body>
 </html>
