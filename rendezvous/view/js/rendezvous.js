100db79b904c5bcbe7b3c5a1deed48e542ae0278
Andrew Manning <tamanning@zoho.com>
2016-09-29 22:12:13 -0400

    Add and delete markers on map. JQueryUI for edit form popup to add name and description.

--- rendezvous/view/js/rendezvous.js
+++ rendezvous/view/js/rendezvous.js
@@ -1,8 +1,10 @@
 // Declare the rendezvous namespace
 var rv = rv || {};
 
+rv.selectedLatLon = {};
+rv.markers = [];
 
-rv.mymap = L.map('map').setView([51.505, -0.09], 13);
+rv.map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
 	maxZoom: 18,
@@ -10,25 +12,95 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 	id: 'mapbox.streets'
-}).addTo(rv.mymap);
+}).addTo(rv.map);
 
-L.marker([51.5, -0.09]).addTo(rv.mymap)
+L.marker([51.5, -0.09]).addTo(rv.map)
 		.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
 
 rv.popup = L.popup();
 
-function onMapClick(e) {
+rv.onMapClick = function (e) {
+	rv.selectedLatLon = e.latlng;
+	window.console.log(e.latlng.toString());
 	rv.popup
 			.setLatLng(e.latlng)
-			.setContent("You clicked the map at " + e.latlng.toString())
-			.openOn(rv.mymap);
+			//.setContent("You clicked the map at " + e.latlng.toString())
+			.setContent(
+				$('#add-marker-button-wrapper').html()
+			)
+			.openOn(rv.map);
+
+	
+$('.add-marker').on('click', rv.createMarker);
+	
 }
 
-rv.mymap.on('click', onMapClick);
+rv.map.on('click', rv.onMapClick);
 
-rv.mymap.addControl( new L.Control.Gps({marker: new L.Marker([0,0])}) );//inizialize control
+rv.map.addControl( new L.Control.Gps({
+	marker: new L.Marker([0,0]).bindPopup("<b>Me</b><br />Current position."),
+	autoActive:false
+}) );//inizialize control
+
+
+
+rv.createMarker = function (e) {
+	var marker = L.marker([rv.selectedLatLon.lat, rv.selectedLatLon.lng]);
+	var id = rv.guid('marker');
+	
+	marker.addTo(rv.map)
+		.bindPopup(rv.editMarkerHTML)
+		.openPopup();
+	marker.on('click', function(){
+		window.console.log('you clicked marker: ' + id);
+		rv.currentMarkerID = id; // global tracker of currently selected marker ID
+	});
+	rv.markers.push({
+		marker: marker,
+		id: id
+	});
+			
+}
+rv.guid = function (prefix) {
+	return prefix+'-xxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+		return v.toString(16);
+	});
+}
+rv.editMarkerHTML = function (id) {
+	setTimeout(function() {
+		$('.edit-marker').on('click', rv.editMarker);
+		$('.delete-marker').on('click', rv.deleteMarker);
+	}, 300);
+		return $('#edit-marker-button-wrapper').html();
+}
+
+rv.editMarker = function (e) {
+	window.console.log('edit marker: ' + rv.currentMarkerID);
+	$( "#edit-marker-form" ).dialog({
+      modal: true,
+      buttons: {
+        Ok: function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+}
+
+rv.deleteMarker = function (e) {
+	window.console.log('delete marker');
+	rv.markers.forEach(function(f) {
+		window.console.log(f.marker._latlng.toString());
+		window.console.log(f.id);
+		if(f.id === rv.currentMarkerID) {
+			window.console.log('Deleting marker!');
+			rv.map.removeLayer(f.marker);
+		}
+	});
+	
+}
+
 
 $(window).load(function () {
-
 
 });
--- rendezvous/view/tpl/rendezvous.tpl
+++ rendezvous/view/tpl/rendezvous.tpl
@@ -4,6 +4,9 @@
 				<title>{{$pagetitle}}</title>
 				<link href="/addon/rendezvous/view/css/jquery-ui.css" rel='stylesheet' type='text/css'>
 				<link href="/addon/rendezvous/view/css/rendezvous.css" rel='stylesheet' type='text/css'>
+				<link href="/library/bootstrap/css/bootstrap.min.css?v=1.13.3" rel='stylesheet' type='text/css'>
+				<link href="/library/font_awesome/css/font-awesome.min.css?v=1.13.3" rel='stylesheet' type='text/css'>
+				
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<link href="/addon/rendezvous/view/css/leaflet.css" rel='stylesheet' type='text/css'>
 				<link href="/addon/rendezvous/view/css/leaflet-gps.css" rel='stylesheet' type='text/css'>
@@ -13,7 +16,25 @@
 		</head>
 		<body>
 				<div id="map" class="map"></div>
+				
+				<div id="add-marker-button-wrapper" style="display: none;">
+						<div><button class="add-marker btn btn-default"><span><i class="fa fa-plus">&nbsp;Add marker</i></span></button></div>
+				</div>
+				
+				<div id="edit-marker-button-wrapper" style="display: none;">
+						<div>
+								<button class="edit-marker btn btn-default btn-sm" title="Edit marker"><span><i class="fa fa-pencil"></i></span></button>
+								<button class="delete-marker btn btn-danger btn-sm" title="Delete marker"><span><i class="fa fa-trash-o"></i></span></button>
+						</div>
+				</div>
 
+				 
+<div id="edit-marker-form" title="Edit Marker">
+  <p>
+    Edit marker info here.
+  </p>
+</div>
+				
 				<script src="/addon/rendezvous/view/js/leaflet.js"></script>
 				<script src="/addon/rendezvous/view/js/leaflet-gps.js"></script>
 				<script src="/addon/rendezvous/view/js/rendezvous.js"></script>
