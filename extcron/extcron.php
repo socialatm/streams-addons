7f01a373a062536b4d63b25821a0ea7ce3754745
zotlabs <mike@macgirvin.com>
2017-01-27 12:24:02 -0800

    issue #657

--- extcron/extcron.php
+++ extcron/extcron.php
@@ -3,9 +3,9 @@
 
 /**
  * Name: external cron
- * Description: Use external server or service to run poller regularly
+ * Description: Use external server or service to run background tasks regularly
  * Version: 1.0
- * Author: Mike Macgirvin <mike@zothub.com>
+ * Author: Mike Macgirvin <mike@macgirvin.com>
  * Maintainer: Mike Macgirvin <mike@macgirvin.com>
  * 
  * Notes: External service needs to make a web request to http(s)://yoursite/extcron
