fd344dea59357d3e4a8bab7f5d042b223fdfc517
zotlabs <mike@macgirvin.com>
2017-03-05 16:15:17 -0800

    move emojione emoticon support to addon (except for a small number of emoji 'reactions' kept in core)

---
+++ emojione/emojione.php
@@ -0,0 +1,40 @@
+<?php
+
+
+/**
+ * Name: Emojione
+ * Description: Emoji emoticons
+ * Version: 1.0
+ *
+ */
+
+
+
+function emojione_load() {
+	\Zotlabs\Extend\Hook::register('smilie','addon/emojione/emojione.php', [ '\\Emojione' , 'smilie' ]);
+}
+
+function emojione_unload() {
+	\Zotlabs\Extend\Hook::unregister('smilie','addon/emojione/emojione.php', [ '\\Emojione' , 'smilie' ]);
+}
+
+
+class Emojione {
+
+	static $listing = null;
+
+	static public function smilie(&$x) {
+
+		if(! self::$listing)
+			self::$listing = json_decode(@file_get_contents('addon/emojione/emoji.json'),true);
+
+		if(self::$listing) {
+			foreach(self::$listing as $lv) {
+				if(strpos($lv['shortname'],':tone') === 0)
+					continue;
+				$x['texts'][] = $lv['shortname'];
+				$x['icons'][] = '<img class="smiley emoji" height="16" width="16" src="addon/emojione/emojis/' . $lv['unicode'] . '.png' . '" alt="' . $lv['name'] . '" />';
+			}
+		}
+	}
+}
