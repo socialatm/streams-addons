ac87f0c133f6318e5d1881eb7e87f2035727ee5c
zotlabs <mike@macgirvin.com>
2017-08-03 23:00:40 -0700

    references

--- pubcrawl/HTTPSig.php
+++ pubcrawl/HTTPSig.php
@@ -3,12 +3,14 @@
 
 class HTTPSig {
 
+	// See RFC5843
 
 	static function generate_digest($body) {
 		$digest = base64_encode(hash('sha256',$body,true));
 		header('Digest: SHA-256=' . $digest);
 	}
 
+	// See draft-cavage-http-signatures-07
 
 	static function sign($head,$body,$prvkey,$keyid = 'key') {
 		$fields = '';
