07f277027c01109fdd3f5242b6e58681f130a79e
Mario Vavti <mario@mariovavti.com>
2017-09-22 21:23:51 +0200

    do not send us back our own comments

--- pubcrawl/Mod_Inbox.php
+++ pubcrawl/Mod_Inbox.php
@@ -54,9 +54,10 @@ class Inbox extends \Zotlabs\Web\Controller {
 
 			if($parent) {
 				//this is a comment - deliver to everybody who owns the parent
-				$channels = q("SELECT * from channel where channel_id in ( SELECT uid from item where ( mid = '%s' || mid = '%s' ) and parent_mid = mid )",
+				$channels = q("SELECT * from channel where channel_id in ( SELECT uid from item where  ( mid = '%s' || mid = '%s' ) and parent_mid = mid ) and channel_address != '%s'",
 					dbesc($parent),
-					dbesc(basename($parent))
+					dbesc(basename($parent)),
+					dbesc(str_replace(z_root() . '/channel/', '', $observer_hash))
 				);
 			}
 			else {
