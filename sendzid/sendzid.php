5ca1665335bf1739f7e39fa3d5db5942c6702f98
zotlabs <mike@macgirvin.com>
2018-01-16 21:19:36 -0800

    update addons which call the get_features hook. They seem to be a bit outdated in a number of ways.

--- sendzid/sendzid.php
+++ sendzid/sendzid.php
@@ -17,13 +17,23 @@ function sendzid_unload() {
 	unregister_hook('get_features','addon/sendzid/sendzid.php','sendzid_get_features');
 }
 
-function sendzid_get_features(&$a,&$b) {
+function sendzid_get_features(&$a,&$x) {
 
-	//FIXME - needs a better description
-	$b['general'][] = array(
-		'sendzid',
-		t('Extended Identity Sharing'),
-		t('Share your identity with all websites on the internet. When disabled, identity is only shared with sites in the matrix.'),false);
+	$entry = [
+		'sendzid',                                                                         
+		t('Extended Identity Sharing'),                                                                      
+		t('Share your identity with all websites on the internet. When disabled, identity is only shared with $Projectname sites.'),
+		false,                                                                              
+		get_config('feature_lock','sendzid'),                                              
+		feature_level('sendzid',4),          
+	];
+
+	if(($x['filtered'] == false) || ($x['filtered'] == true && intval($x['techlevel']) >= intval($entry[5]))) {
+		if(! is_array($x['features']['general'])) {
+			$x['features']['general'] = [ t('General Features') ];
+		}
+		$x['features']['general'][] = $entry;
+	}
 
 }
 
