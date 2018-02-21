9a6bca03d0ce95de73272ef6a77574b18e08035e
zotlabs <mike@macgirvin.com>
2018-02-20 21:06:07 -0800

    remove the friendica protocol from nodeinfo until it is fully implemented

--- statistics/nodeinfo.php
+++ statistics/nodeinfo.php
@@ -66,10 +66,10 @@ function nodeinfo_content(&$a) {
 			$arr['protocols']['outbound'][] = 'gnusocial';
 		}
 
-		if(in_array('friendica',App::$plugins)) {
-			$arr['protocols']['inbound'][] = 'friendica';
-			$arr['protocols']['outbound'][] = 'friendica';
-		}
+//		if(in_array('friendica',App::$plugins)) {
+//			$arr['protocols']['inbound'][] = 'friendica';
+//			$arr['protocols']['outbound'][] = 'friendica';
+//		}
 
 		$services = array();
 		$iservices = array();
@@ -139,9 +139,9 @@ function nodeinfo_content(&$a) {
 			$arr['protocols'][] = 'ostatus';
 		}
 
-		if(in_array('friendica',App::$plugins)) {
-			$arr['protocols'][] = 'friendica';
-		}
+//		if(in_array('friendica',App::$plugins)) {
+//			$arr['protocols'][] = 'friendica';
+//		}
 
 		if(in_array('pubcrawl',App::$plugins)) {
 			$arr['protocols'][] = 'activitypub';
