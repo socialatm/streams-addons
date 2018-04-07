f8c494e926da48748eed2930f23afb1848a9a500
DM42.Net (Matt Dent) <dentm42@dm42.net>
2018-04-07 16:07:49 -0400

    Remove parameter from cart_install() to reflect 3.x conventions

--- cart/cart.php
+++ cart/cart.php
@@ -946,7 +946,7 @@ function cart_setcartconfig($param,$val) {
 	return set_pconfig(local_channel(),"cart",$param);
 }
 
-function cart_install(&$a) {
+function cart_install() {
 		logger ('[cart] Install start.',LOGGER_DEBUG);
 	if (cart_dbUpgrade () == UPDATE_FAILED) {
 		notice ('[cart] Install error - Abort installation.');
