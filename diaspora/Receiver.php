socialatm/streams-addons

commit fc1db4831dca49a4403fd712d2e89dec33361d6e

remove support for old style forum tags in Diaspora protocol

diaspora/Receiver.php
@@ -355,8 +355,6 @@ class Diaspora_Receiver {
 		$cnt = preg_match_all('/@\[zrl=(.*?)\](.*?)\[\/zrl\]/ism',$body,$matches,PREG_SET_ORDER);
 		if($cnt) {
 			foreach($matches as $mtch) {
-				// don't include plustags in the term
-				$term = ((substr($mtch[2],-1,1) === '+') ? substr($mtch[2],0,-1) : $mtch[2]);
 				$datarray['term'][] = [
 					'uid'   => $this->importer['channel_id'],
 					'ttype' => TERM_MENTION,
@@ -609,7 +607,6 @@ class Diaspora_Receiver {
 		if($cnt) {
 			foreach($matches as $mtch) {
 				// don't include plustags in the term
-				$term = ((substr($mtch[2],-1,1) === '+') ? substr($mtch[2],0,-1) : $mtch[2]);
 				$datarray['term'][] = array(
 					'uid'   => $this->importer['channel_id'],
 					'ttype'  => TERM_MENTION,
@@ -885,8 +882,6 @@ class Diaspora_Receiver {
 		$cnt = preg_match_all('/@\[zrl=(.*?)\](.*?)\[\/zrl\]/ism',$body,$matches,PREG_SET_ORDER);
 		if($cnt) {
 			foreach($matches as $mtch) {
-				// don't include plustags in the term
-				$term = ((substr($mtch[2],-1,1) === '+') ? substr($mtch[2],0,-1) : $mtch[2]);
 				$datarray['term'][] = [
 					'uid'   => $this->importer['channel_id'],
 					'ttype' => TERM_MENTION,

commit fccbd51c08ee1b6be01eb3bb571eebfafb113944

fix issues introduced with commit fc1db4831dca49a4403fd712d2e89dec33361d6e

diaspora/Receiver.php
@@ -359,7 +359,7 @@ class Diaspora_Receiver {
 					'uid'   => $this->importer['channel_id'],
 					'ttype' => TERM_MENTION,
 					'otype' => TERM_OBJ_POST,
-					'term'  => $term,
+					'term'  => $mtch[2],
 					'url'   => $mtch[1]
 				];
 			}
@@ -381,13 +381,11 @@ class Diaspora_Receiver {
 		$cnt = preg_match_all('/\!\[zrl=(.*?)\](.*?)\[\/zrl\]/ism',$body,$matches,PREG_SET_ORDER);
 		if($cnt) {
 			foreach($matches as $mtch) {
-				// don't include plustags in the term
-				$term = ((substr($mtch[2],-1,1) === '+') ? substr($mtch[2],0,-1) : $mtch[2]);
 				$datarray['term'][] = [
 					'uid'   => $this->importer['channel_id'],
 					'ttype' => TERM_FORUM,
 					'otype' => TERM_OBJ_POST,
-					'term'  => $term,
+					'term'  => $mtch[2],
 					'url'   => $mtch[1]
 				];
 			}