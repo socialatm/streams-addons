374c7ed86513a1a767833428525a4541d8136a20
Mario Vavti <mario@mariovavti.com>
2018-05-06 14:32:43 +0200

    pubcrawl: in a private reply if author == owner tag everyone in the acl otherwise just tag the owner.

--- pubcrawl/as.php
+++ pubcrawl/as.php
@@ -448,11 +448,17 @@ function asencode_activity($i) {
 		$ret['to'] = [ $reply_url ];
 
 		if($i['item_private']) {
-			$ret['tag'] = [
-				'type' => 'Mention',
-				'href' => $reply_url,
-				'name' => '@' . $reply_addr
-			];
+			if($i['author_xchan'] == $i['owner_xchan']) {
+				$m = as_map_acl($i,true);
+			}
+			else {
+				$m = [
+					'type' => 'Mention',
+					'href' => $reply_url,
+					'name' => '@' . $reply_addr
+				];
+			}
+			$ret['tag'] = (($ret['tag']) ? array_merge($ret['tag'],$m) : $m);
 		}
 	}
 	else {
