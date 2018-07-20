44b0956753e8b5ab351288367733b3bdc8c731ad
zotlabs <mike@macgirvin.com>
2018-07-19 20:45:02 -0700

    add zotpost app

---
+++ zotpost/Mod_zotpost.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Zotlabs\Module;
+
+use Zotlabs\Lib\Apps;
+
+
+class Zotpost extends \Zotlabs\Web\Controller {
+
+	function post() {
+
+		if(! ( local_channel() && Apps::addon_app_installed(local_channel(),'zotpost'))) { 
+			return;
+		}
+
+
+
+		$channel = \App::get_channel();
+
+		// Don't let somebody post to their self channel. Since we aren't passing message-id this would be very very bad.
+
+		if(! trim($_POST['zotpost_channel'])) {
+			notice( t('Channel is required.') . EOL);
+			return;
+		}
+
+		if($channel['channel_address'] === trim($_POST['zotpost_channel'])) {
+			notice( t('Invalid channel.') . EOL);
+			return;
+		}
+
+		set_pconfig(local_channel(), 'zotpost', 'baseapi',         trim($_POST['zotpost_baseapi']));
+		set_pconfig(local_channel(), 'zotpost', 'password',        z_obscure(trim($_POST['zotpost_password'])));
+		set_pconfig(local_channel(), 'zotpost', 'channel',         trim($_POST['zotpost_channel']));
+		set_pconfig(local_channel(), 'zotpost', 'post',            intval($_POST['zotpost_enable']));
+		set_pconfig(local_channel(), 'zotpost', 'post_by_default', intval($_POST['zotpost_default']));
+        info( t('Zotpost Settings saved.') . EOL);
+
+
+	}
+
+	function get() {
+
+		$desc = t('This addon app allows you to cross-post to other Zot services and channels.');
+
+		$text =  '<div class="section-content-info-wrapper">' . $desc . '</div>';
+
+
+		if(! ( local_channel() && Apps::addon_app_installed(local_channel(),'zotpost'))) {
+			return $text;
+		}
+
+		nav_set_selected(t('Zotpost'));
+
+		$api     = get_pconfig(local_channel(), 'zotpost', 'baseapi');
+		$password = z_unobscure(get_pconfig(local_channel(), 'zotpost', 'password' ));
+		$channel = get_pconfig(local_channel(), 'zotpost', 'channel' );
+		$defenabled = get_pconfig(local_channel(),'zotpost','post_by_default');
+		$defchecked = (($defenabled) ? 1 : false);
+
+
+		$sc = $text;
+
+		$sc .= replace_macros(get_markup_template('field_checkbox.tpl'), array(
+			'$field'	=> array('zotpost_default', t('Send public postings to Zot channel by default'), $defchecked, '', array(t('No'),t('Yes'))),
+		));
+
+		$sc .= replace_macros(get_markup_template('field_input.tpl'), array(
+			'$field'	=> array('zotpost_baseapi', t('Zot API Path'), $api, t('https://{sitename}/api'))
+		));
+
+		$sc .= replace_macros(get_markup_template('field_input.tpl'), array(
+			'$field'	=> array('zotpost_channel', t('Zot channel name'), $channel, t('Nickname'))
+		));
+
+		$sc .= replace_macros(get_markup_template('field_password.tpl'), array(
+			'$field'	=> array('zotpost_password', t('Zot password'), $password, '')
+		));
+
+		return replace_macros(get_markup_template('generic_app_settings.tpl'), array(
+			'$addon' 	=> array('zotpost', t('Zotpost Settings'), '', t('Submit')),
+			'$content'	=> $sc
+		));
+
+	}
+
+}
---
+++ zotpost/zotpost.php
@@ -0,0 +1,118 @@
+<?php
+
+use Zotlabs\Extend\Hook;
+use Zotlabs\Extend\Route;
+use Zotlabs\Lib\Apps;
+
+/**
+ * Name: Zot Crosspost Connector (zotpost)
+ * Description: Relay public postings to another Zot or Zot6 channel
+ * Version: 1.0
+ * Maintainer: none
+ */
+ 
+/*
+ *   Hubzilla to Hubzilla
+ */
+
+require_once('include/permissions.php');
+
+function zotpost_load() {
+
+	Hook::register('notifier_normal', 'addon/zotpost/zotpost.php', 'zotpost_post_hook');
+	Hook::register('post_local', 'addon/zotpost/zotpost.php', 'zotpost_post_local');
+	Hook::register('jot_networks',    'addon/zotpost/zotpost.php', 'zotpost_jot_nets');
+	Route::register('addon/zotpost/Mod_zotpost.php','zotpost');
+}
+
+
+function zotpost_unload() {
+	Hook::unregister('notifier_normal', 'addon/zotpost/zotpost.php', 'zotpost_post_hook');
+	Hook::unregister('post_local', 'addon/zotpost/zotpost.php', 'zotpost_post_local');
+	Hook::unregister('jot_networks',    'addon/zotpost/zotpost.php', 'zotpost_jot_nets');
+	Route::unregister('addon/zotpost/Mod_zotpost.php','zotpost');
+
+}
+
+function zotpost_jot_nets(&$b) {
+    if((! local_channel()) || (! perm_is_allowed(local_channel(),'','view_stream')))
+        return;
+
+	if(Apps::addon_app_installed(local_channel(),'zotpost')) {
+		$zotpost_defpost = get_pconfig(local_channel(),'zotpost','post_by_default');
+		$selected = ((intval($zotpost_defpost) == 1) ? ' checked="checked" ' : '');
+		$b .= '<div class="profile-jot-net"><input type="checkbox" name="zotpost_enable"' . $selected . ' value="1" /> ' 
+			. '<img src="images/rm-32.png" /> ' . t('Post to Zot') . '</div>';
+	}
+}
+
+
+function zotpost_post_local(&$b) {
+	if($b['created'] != $b['edited'])
+		return;
+
+	if(! perm_is_allowed($b['uid'],'','view_stream'))
+		return;
+
+	if((local_channel()) && (local_channel() == $b['uid']) && (! $b['item_private'])) {
+
+		$zotpost_post = Apps::addon_app_installed(local_channel(),'zotpost');
+		$zotpost_enable = (($zotpost_post && x($_REQUEST,'zotpost_enable')) ? intval($_REQUEST['zotpost_enable']) : 0);
+
+		// if API is used, default to the chosen settings
+		if($_REQUEST['api_source'] && intval(get_pconfig(local_channel(),'zotpost','post_by_default')))
+			$zotpost_enable = 1;
+
+		if(! $zotpost_enable)
+			return;
+
+       if(strlen($b['postopts']))
+           $b['postopts'] .= ',';
+       $b['postopts'] .= 'zotpost';
+    }
+}
+
+
+function zotpost_post_hook(&$b) {
+
+	/**
+	 * Post to Zot
+	 */
+
+	// for now, just top level posts.
+
+	if($b['mid'] != $b['parent_mid'])
+		return;
+
+	if((! is_item_normal($b)) || $b['item_private'] || ($b['created'] !== $b['edited']))
+		return;
+
+	if(! perm_is_allowed($b['uid'],'','view_stream'))
+		return;
+
+	if(! strstr($b['postopts'],'zotpost'))
+		return;
+
+	logger('zotpost invoked');
+
+	load_pconfig($b['uid'], 'zotpost');
+
+	
+	$api      = get_pconfig($b['uid'], 'zotpost', 'baseapi');
+	if(substr($api,-1,1) != '/')
+		$api .= '/';
+
+	$password = z_unobscure(get_pconfig($b['uid'], 'zotpost', 'password'));
+	$channel  = get_pconfig($b['uid'], 'zotpost', 'channel');
+
+	$postdata = array('body' => $b['body'], 'title' => $b['title'], 'source' => (($b['app']) ? : 'ZAP/ZotPost'));
+
+	if(strlen($b['body'])) {
+		$ret = z_post_url($api . '/red/item/update', $postdata, 0, array('http_auth' => $channel . ':' . $password));
+		if($ret['success'])
+			logger('zotpost: returns: ' . print_r($ret['body'],true));
+		else
+			logger('zotpost: z_post_url failed: ' . print_r($ret['debug'],true));
+	}
+}
+
