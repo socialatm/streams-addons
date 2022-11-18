6bcb6f65cd036543bee7c0ca0fd4f34a1e986922
Mike Macgirvin <mike@macgirvin.com>
2022-11-19 08:35:39 +1100

    refactor followlist

--- followlist/Mod_followlist.php
+++ followlist/Mod_followlist.php
@@ -7,137 +7,153 @@ use Code\Lib\ASCollection;
 use Code\Lib\Connect;
 use Code\Lib\Apps;
 use Code\Lib\ServiceClass;
-use Code\Render\Theme;                                                                                                                                            
-
-    
-class Followlist extends Controller {
-
-	function post() {
-
-		if (! local_channel()) {
-			return;
-		}
-
-		if (! Apps::addon_app_installed(local_channel(),'followlist')) {
-			return;
-		}
-
-		check_form_security_token_redirectOnErr('/stream', 'followlist');
-
-		$max_records = get_config('system','max_imported_channels',1000);
-		$importer = App::get_channel();
-		$url = $_GET['url'];
+use Code\Render\Theme;
 
 
-		$obj = null;
-		$src = null;
-		$actors = [];
+class Followlist extends Controller
+{
 
-		if(($_FILES) && array_key_exists('userfile',$_FILES) && intval($_FILES['userfile']['size'])) {
-			$filename = $_FILES['userfile']['name'];
-			$src = $_FILES['userfile']['tmp_name'];
-			$type = $_FILES['userfile']['type'];
-		}
+    public function post()
+    {
+        $actors = [];
 
-		if ($type === 'text/csv') {
-			$lines = file($src);
-			if ($lines) {
-				array_shift($lines);
-			}
-			if ($lines) {
-				foreach ($lines as $line) {
-					$csv = str_getcsv($line);
-					if ($csv) {
-						$actors[] = $csv[0];
-					}
-				}
-			}
-			$actors = array_slice($actors,0,$max_records);
-		}
-		else {
-			$obj = json_decode(file_get_contents($src),true);
-		}
+        if (!local_channel()) {
+            return;
+        }
 
-		if ($src) {
-			unlink($src);
-		}
+        if (!Apps::addon_app_installed(local_channel(), 'followlist')) {
+            return;
+        }
 
-		if ($importer && ($url || $actors)) {
+        check_form_security_token_redirectOnErr('/stream', 'followlist');
 
-			if (! $obj) {
-				$obj = new ASCollection($url, $importer, 0, $max_records);
-			}
-			if (! $actors) {
-				$actors = $obj->get();
-			}
+        $max_records = get_config('system', 'max_imported_channels', 1000);
+        $importer = App::get_channel();
+        if (! $importer) {
+            notice('Unable to locate channel');
+            return;
+        }
 
-			if ($actors) {
-				notice( t('Adding connections') . EOL);
-				
-				foreach ($actors as $actor) {
-					if (get_config('system','followlist_test',false)) {
-						logger('followlist: ' . $actor);
-						continue;
-					}
-					if (is_array($actor)) {
-						$result = Connect::connect($importer,$actor['id']);
-					}
-					else {
-						$result = Connect::connect($importer,$actor);
-					}
-					if (! $result['success']) {
-						notice ( t('Connect failed: ') . $url . t(' Reason: ') . $result['message']);
-					}
-				}
-				notice( t('Finished adding connections') . EOL);
-			}
-		}
-	}
+        $url = $_REQUEST['url'];
 
-	function get() {
+        $src = null;
+
+        $type = '';
+        $collection = false;
+
+        if (($_FILES) && array_key_exists('userfile', $_FILES) && intval($_FILES['userfile']['size'])) {
+            $src = $_FILES['userfile']['tmp_name'];
+            $type = $_FILES['userfile']['type'];
+        }
+
+        if ($src && $type === 'text/csv') {
+            $actors = $this->fetchFromFile($src, $max_records);
+        }
+        elseif ($src) {
+            $collection = $this->fetchFromURL($src);
+        }
+        else {
+            $collection = $url;
+        }
+
+        if ($src) {
+            unlink($src);
+        }
+
+        if ($collection) {
+            $obj = new ASCollection($collection, $importer, 0, $max_records);
+            $actors = $obj->get();
+        }
+
+        if ($actors) {
+            notice(t('Adding connections') . EOL);
+
+            foreach ($actors as $actor) {
+                if (get_config('system', 'followlist_test', false)) {
+                    logger('followlist: ' . $actor);
+                    continue;
+                }
+                $result = Connect::connect($importer, is_array($actor) ? $actor['id'] : $actor);
+                if (!$result['success']) {
+                    notice(t('Connect failed: ') . $result['message']);
+                }
+            }
+            notice(t('Finished adding connections') . EOL);
+        }
+        else {
+            notice (t('No entries found.'));
+        }
+    }
+
+    public function get()
+    {
 
         $desc = t('This app allows you to connect to everybody in a pre-defined ActivityPub collection or CSV file, such as follower/following lists. Install the app and revisit this page to input the source URL.');
 
         $text = '<div class="section-content-info-wrapper">' . $desc . '</div>';
 
-		if (! local_channel()) {
-			return login();
-		}
+        if (!local_channel()) {
+            return login();
+        }
 
 
-		if (! Apps::addon_app_installed(local_channel(),'followlist')) {
-			return $text;
-		}
+        if (!Apps::addon_app_installed(local_channel(), 'followlist')) {
+            return $text;
+        }
 
-		$max_records = get_config('system','max_imported_channels',1000);
+        $max_records = get_config('system', 'max_imported_channels', 1000);
 
-		// check service class limits
+        // check service class limits
+        $total_channels = 0;
+        $r = q("select count(*) as total from abook where abook_channel = %d and abook_self = 0 ",
+            intval(local_channel())
+        );
+        if ($r) {
+            $total_channels = $r[0]['total'];
+            $sc = ServiceClass::fetch(local_channel(), 'total_channels');
+            if ($sc !== false) {
+                $allowed = intval($sc) - $total_channels;
+                if ($allowed < $max_records) {
+                    $max_records = $allowed;
+                }
+            }
+        }
 
-		$r = q("select count(*) as total from abook where abook_channel = %d and abook_self = 0 ",
-			intval(local_channel())
-		);
-		if ($r) {
-			$total_channels = $r[0]['total'];
-		}
+        return replace_macros(Theme::get_template('followlist.tpl', 'addon/followlist'), [
+            '$page_title' => t('Followlist'),
+            '$limits' => sprintf(t('You may import up to %d records'), $max_records),
+            '$form_security_token' => get_form_security_token("followlist"),
+            '$disabled' => (($total_channels > $max_records) ? ' disabled="disabled" ' : EMPTY_STR),
+            '$notes' => t('Enter the URL of an ActivityPub followers/following collection to import'),
+            '$upload' => t('Or upload an ActivityPub or CSV followers file from your device'),
+            '$url' => ['url', t('URL of followers/following list'), '', ''],
+            '$submit' => t('Submit')
+        ]);
 
-		$sc = ServiceClass::fetch(local_channel(),'total_channels');
-		if ($sc !== false) {
-			$allowed = intval($sc) - $total_channels;
-			if ($allowed < $max_records) {
-				$max_records = $allowed;
-			}
-		}
+    }
 
-		return replace_macros(Theme::get_template('followlist.tpl','addon/followlist'), [
-			'$page_title'          => t('Followlist'),
-			'$limits'              => sprintf( t('You may import up to %d records'), $max_records), 
-			'$form_security_token' => get_form_security_token("followlist"),
-			'$disabled'            => (($total_channels > $max_records) ? ' disabled="disabled" ' : EMPTY_STR),
-			'$notes'               => t('Enter the URL of an ActivityPub followers/following collection to import'),
-			'$upload'              => t('Or upload an ActivityPub or CSV followers file from your device'),
-			'$url'                 => [ 'url', t('URL of followers/following list'), '', '' ],
-			'$submit'              => t('Submit')
-		]);
+    protected function fetchFromFile($src, $maxRecords)
+    {
+        $actors = [];
+        $lines = file($src);
+        if ($lines) {
+            array_shift($lines);
+        }
+        if ($lines) {
+            foreach ($lines as $line) {
+                $csv = str_getcsv($line);
+                if ($csv) {
+                    $actors[] = $csv[0];
+                }
+            }
+        }
+        return array_slice($actors, 0, $maxRecords);
+    }
 
-	}
+    protected function fetchFromURL($src)
+    {
+        return json_decode(file_get_contents($src), true);
+    }
+
+
 }
